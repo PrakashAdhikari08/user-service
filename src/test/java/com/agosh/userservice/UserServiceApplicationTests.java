9bdd16044a4bf12ba17c2e679f3edc5fb4dae705
PrakashAdhikari08 <prakashadhikarijhapali@gmail.com>
2022-06-22 10:15:49 +1000

    test fixing

--- src/test/java/com/agosh/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/agosh/userservice/UserServiceApplicationTests.java
@@ -2,6 +2,7 @@ package com.agosh.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("local")
